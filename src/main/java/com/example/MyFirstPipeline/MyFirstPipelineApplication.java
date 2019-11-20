57022d374c6c77349c4d79ea8acd75bd206ce5e9
Akanksha2795 <kapureakanksha21@gmail.com>
2019-11-20 04:08:18 -0800

    first commit

---
+++ src/main/java/com/example/MyFirstPipeline/MyFirstPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstPipelineApplication.class, args);
+	}
+
+}
